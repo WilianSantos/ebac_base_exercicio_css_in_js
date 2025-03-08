d4aa65634cce7b431224ed217c54e9913dc403d7
Wilian <wilian.santos3@outlook.com>
2025-03-08 09:31:46 -0300

    Instalação do styled-components e implementação do arquivo raiz do estilo global

--- src/style.ts
+++ src/style.ts
@@ -1,4 +1,7 @@
-:root {
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+ :root {
   --cor-principal: #a7727d;
   --cor-secundaria: #f9f5e7;
 }
@@ -15,14 +18,15 @@ body {
   padding-bottom: 120px;
 }
 
-.container {
+}
+`
+
+export const Container = styled.div`
   max-width: 1024px;
   width: 100%;
   margin: 0 auto;
-}
 
-@media (max-width: 1024px) {
-  .container {
+  @media (max-width: 1024px) {
     max-width: 80%;
   }
-}
+`
