b825bdf189d043aae9e1669e9bbe405ea10be61e
Wilian <wilian.santos3@outlook.com>
2025-03-08 10:13:17 -0300

    Implementação da hero com styled-components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,14 @@
-import styles from './Hero.module.css'
+import { Container } from '../../style'
+import { HeroForm, HeroTitle } from './style'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <HeroForm>
+    <Container>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </Container>
+  </HeroForm>
 )
 
 export default Hero
